5f9bc85c7b14fac307376b0db77d436222f1f9c0
Steven Feltner <57229654+stevenfeltner@users.noreply.github.com>
2023-07-31 19:18:38 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "azurerm_public_ip" "pip" {
   sku                 = var.public_ip_sku
   sku_tier            = var.public_ip_sku_tier
   domain_name_label   = var.domain_name_label
-  zone                = var.public_ip_availability_zone
+  zones                = var.public_ip_availability_zone
   tags                = merge({ "ResourceName" = lower("pip-vm-${var.virtual_machine_name}-${data.azurerm_resource_group.rg.location}-0${count.index + 1}") }, var.tags, )
 
   lifecycle {
